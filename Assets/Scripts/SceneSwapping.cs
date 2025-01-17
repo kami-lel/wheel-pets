7400c93f7285ca10837b7b2f5204e19a4e0233f0
Yazmyn Sims <122718475+YazmynS@users.noreply.github.com>
2025-01-17 14:01:54 -0800

    Separate Driving Scene from Pet Scene

---
+++ Assets/Scripts/SceneSwapping.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwapping : MonoBehaviour
+{
+   //Load GameScene
+   public void LoadDrivingGameScene()
+   {
+       SceneManager.LoadScene("DrivingGameScene");
+   }
+
+    public void LoadPetGameScene()
+   {
+       SceneManager.LoadScene("PetGameScene");
+   }
+
+   public void LoadTitleScene()
+   {
+       SceneManager.LoadScene("TitleScene");
+   }
+   public void LoadLeaderboardScene()
+   {
+       SceneManager.LoadScene("LeaderboardScene");
+   }
+   public void LoadOptionsScene(){
+         SceneManager.LoadScene("OptionsScene");
+   }
+   public void ExitGame(){
+        Application.Quit();
+   }
+
+}
