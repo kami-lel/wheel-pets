f231aa3de8db033b6a364315390d4a5e0bf87f24
emilsabev2003 <150591482+emilsabev2003@users.noreply.github.com>
2025-01-18 19:21:44 -0800

    comments added to button scaler script

--- Assets/Scripts/ButtonScaler.cs
+++ Assets/Scripts/ButtonScaler.cs
@@ -3,12 +3,12 @@
 using UnityEngine;
 public class ButtonScaler : MonoBehaviour
 {
-    public void PointerEnter()
+    public void PointerEnter() //increase button scale when pointer enters button box
     {
         transform.localScale = new Vector2(0.52f, 0.6866667f);
     }
 
-    public void PointerExit()
+    public void PointerExit() //decrease button scale when pointer leaves button box
     {
         transform.localScale = new Vector2(0.5f, 0.6666667f);
     }
