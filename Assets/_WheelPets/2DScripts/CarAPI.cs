870dcd08ee72e54789d0b8806fe6d1030d4a23a6
kami-lel <77594253+kami-lel@users.noreply.github.com>
2025-01-20 01:47:30 -0800

    finish backbone of CarAPI

--- Assets/_WheelPets/2DScripts/CarAPI.cs
+++ Assets/_WheelPets/2DScripts/CarAPI.cs
@@ -1,19 +1,76 @@
 /* 
-  CarAPI define an interace between the pet-game
+  CarAPI define an interface between the pet-game
   and the "car" (i.e. the driving sim)
 
   TODO write how to use on both sides
 */
 
 
-using UnityEngine;
+using System;
+using System.Collections.Generic;
 
 
 namespace CarAPI
 {
-    public class CarEventManager
-    {
-        // TODO
+    public enum CarEventID
+    { 
+        // Car related event
+        ShiftIntoPark,
+        ShiftIntoDrive,
+        ShiftIntoReverse,
+        StartCharging,  // consider StartCharing as another gear
 
+        // car battery related events
+        BatteryEmpty,
+        BatteryLowPower,
+        BatteryFull,
+
+        // events during driving
+        // can be used to invoke pet animation
+        Accelerate,
+        Decelerate,
+        TurnLeft,
+        TurnRight,
+        SuddenAccelerate,
+        SuddenDecelerate,
+        SuddenTurnLeft,
+        SuddenTurnRight,
+
+        // driver behavior events, used for point scoring
+        // TODO this is just some example, will be changed
+        RunRedLight,
+        FailToStopAtStopSign,
+        HitACyclist,
+    }
+
+    public class CarEventArgs: CarEventArgs { // TODO
+    }
+
+public static class CarEvent
+{
+    private static Dictionary<CarEventID, Action<CarEventArgs>> eventDict =
+        new Dictionary<CarEventID, Action<CarEventArgs>>();
+
+    public static void add(CarEventID id, Action<CarEventArgs> listener) {
+        if (!eventDict.ContainsKey(id))
+        {
+            eventDict[id] = delegate {};
+        }
+        eventDict[id] += listener;
+    }
+
+    public static void remove_all() {
+        foreach (var key in eventDict.Keys.ToList())
+        {
+            eventDict[key] = delegate {};  // reset the action to an empty delegate
+        }
+    }
+
+    public static void emit(CarEventID id, CarEventArgs eventArgs)
+    {
+        if (eventDict.TryGetValue(id, out var action))
+        {
+            action.Invoke(eventArgs);
+        }
     }
 }
