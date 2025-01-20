88e6db8de739776dcbcc95d5c583b51296178cba
kami-lel <77594253+kami-lel@users.noreply.github.com>
2025-01-20 01:06:39 -0800

    implement CarAPI as event manager

---
+++ Assets/_WheelPets/2DScripts/CarAPI.cs
@@ -0,0 +1,19 @@
+/* 
+  CarAPI define an interace between the pet-game
+  and the "car" (i.e. the driving sim)
+
+  TODO write how to use on both sides
+*/
+
+
+using UnityEngine;
+
+
+namespace CarAPI
+{
+    public class CarEventManager
+    {
+        // TODO
+
+    }
+}
