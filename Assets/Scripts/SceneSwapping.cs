f31454eb3f9afcf11edf82f6eb705d51559bd721
joshwidjaja <josh@widjaja.us>
2025-01-19 17:34:06 -0800

    Added blank closet scene

--- Assets/Scripts/SceneSwapping.cs
+++ Assets/Scripts/SceneSwapping.cs
@@ -3,40 +3,55 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwapping : MonoBehaviour
 {
-   //Load GameScene
-   public void LoadDrivingGameScene()
-   {
-       SceneManager.LoadScene("DrivingGameScene");
-   }
+    //Load GameScene
+    public void LoadDrivingGameScene()
+    {
+        SceneManager.LoadScene("DrivingGameScene");
+    }
 
     public void LoadPetGameScene()
-   {
-       SceneManager.LoadScene("PetGameScene");
-   }
+    {
+        SceneManager.LoadScene("PetGameScene");
+    }
 
-   public void LoadTitleScene()
-   {
-       SceneManager.LoadScene("TitleScene");
-   }
-   
-   public void LoadLeaderboardScene()
-   {
-       SceneManager.LoadScene("LeaderboardScene");
-   }
-   
-   public void LoadOptionsScene(){
-         SceneManager.LoadScene("OptionsScene");
-   }
-   
-   public void LoadDogCareScene()
-   {
+    public void LoadTitleScene()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+
+    public void LoadLeaderboardScene()
+    {
+        SceneManager.LoadScene("LeaderboardScene");
+    }
+
+    public void LoadOptionsScene()
+    {
+        SceneManager.LoadScene("OptionsScene");
+    }
+
+    public void LoadClosetScene()
+    {
+        SceneManager.LoadScene("ClosetScene");
+    }
+    public void LoadMinigameSelectorSecene()
+    {
+        SceneManager.LoadScene("MinigameSelectorScene");
+    }
+    public void LoadDogCareScene()
+    {
         SceneManager.LoadScene("DogWalkScene");
-   }
-   
-   public void ExitGame(){
+    }
+
+    public void LoadDogBathScene()
+    {
+        SceneManager.LoadScene("DogBathScene");
+    }
+
+    public void ExitGame()
+    {
         Application.Quit();
-   }
-   
+    }
+
 
 
 }
