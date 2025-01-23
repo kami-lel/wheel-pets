5c90a67ef6172aa3f338e7435a3dc331bdd0a5dc
Yazmyn Sims <122718475+YazmynS@users.noreply.github.com>
2025-01-23 15:31:59 -0800

    Bath Game Updates

--- Assets/_WheelPets/2DScripts/BathGame.cs
+++ Assets/_WheelPets/2DScripts/BathGame.cs
@@ -1,61 +1,151 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using TMPro;
 
-public class BathGame : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+public class BathGame : MonoBehaviour
 {
-    [SerializeField] private GameObject targetObject; // The object to check for collisions with
-    private RectTransform buttonRectTransform; // The RectTransform of the button
-    private Canvas canvas; // The parent Canvas
-    private bool isDragging = false;
+    [SerializeField] public GameObject targetObject; // The object to check for collisions with
+    [SerializeField] public TextMeshProUGUI mistakeText; // Text to display mistakes
+    [SerializeField] public TextMeshProUGUI messageText; // Text to display messages
+
+    // Order booleans
+    private bool isBrushUsed = false;
+    private bool isClippersUsed = false;
+    private bool isSoapUsed = false;
+    private bool isWaterUsed = false;
+    private bool isTowelUsed = false;
+    private bool isScissorsUsed = false;
 
     void Start()
     {
-        buttonRectTransform = GetComponent<RectTransform>();
-        canvas = GetComponentInParent<Canvas>();
-
         if (targetObject == null)
         {
             Debug.LogError("Target object not assigned in the Inspector!");
         }
-    }
 
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        // Start dragging when the pointer is pressed on the button
-        isDragging = true;
-    }
-
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        // Stop dragging when the pointer is released
-        isDragging = false;
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        // If dragging, update the position of the button
-        if (isDragging)
+        if (messageText == null || mistakeText == null)
         {
-            Vector2 position;
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(
-                canvas.transform as RectTransform,
-                eventData.position,
-                canvas.worldCamera,
-                out position
-            );
-            buttonRectTransform.localPosition = position;
+            Debug.LogError("TextMeshProUGUI components not assigned in the Inspector!");
         }
     }
 
-    void Update()
+    public void NotifyItemDragged(string itemTag)
     {
-        if (targetObject == null) return;
+        GameObject draggedItem = GameObject.FindWithTag(itemTag);
 
-        // Check for collision with the target object
-        RectTransform targetRect = targetObject.GetComponent<RectTransform>();
-        if (targetRect != null && RectTransformUtility.RectangleContainsScreenPoint(targetRect, buttonRectTransform.position, canvas.worldCamera))
+        while (mistakeText.text != "XXX" && !isScissorsUsed) // Check if mistakes are less than 3 and towel is not used
         {
-            Debug.Log("Button collided with the target object!");
+            if (itemTag == "brush")
+            {
+                if (!isBrushUsed)
+                {
+                    isBrushUsed = true;
+                    DisplayMessage("The dog is brushed and looking tidy.");
+                    RemoveItem(draggedItem);
+                }
+                else
+                {
+                    DisplayMistake("X");
+                    DisplayMessage("You can't use that yet.");
+                }
+            }
+            else if (itemTag == "clippers")
+            {
+                if (isBrushUsed && !isClippersUsed)
+                {
+                    isClippersUsed = true;
+                    DisplayMessage("The dog has been clipped.");
+                    RemoveItem(draggedItem);
+                }
+                else
+                {
+                    DisplayMistake("X");
+                    DisplayMessage("You can't use that yet.");
+                }
+            }
+            else if (itemTag == "soap")
+            {
+                if (isBrushUsed && isClippersUsed && !isSoapUsed)
+                {
+                    isSoapUsed = true;
+                    DisplayMessage("The dog is lathered.");
+                    RemoveItem(draggedItem);
+                }
+                else
+                {
+                    DisplayMistake("X");
+                    DisplayMessage("You can't use that yet.");
+                }
+            }
+            else if (itemTag == "water")
+            {
+                if (isBrushUsed && isClippersUsed && isSoapUsed && !isWaterUsed)
+                {
+                    isWaterUsed = true;
+                    DisplayMessage("The dog is rinsed.");
+                    RemoveItem(draggedItem);
+                }
+                else
+                {
+                    DisplayMistake("X");
+                    DisplayMessage("You can't use that yet.");
+                }
+            }
+            else if (itemTag == "towel")
+            {
+                if (isWaterUsed && !isTowelUsed)
+                {
+                    isTowelUsed = true;
+                    DisplayMessage("The dog is dried off.");
+                    RemoveItem(draggedItem);
+                }
+                else
+                {
+                    DisplayMistake("X");
+                    DisplayMessage("You can't use that yet.");
+                }
+            }
+            else if (itemTag == "scissors")
+            {
+                if(isTowelUsed && !isScissorsUsed)
+                {
+                    isScissorsUsed = true;
+                    DisplayMessage("All done");
+                    RemoveItem(draggedItem);
+                }
+                else
+                {
+                    DisplayMistake("X");
+                    DisplayMessage("You can't use that yet.");
+                }
+            }
         }
+    }
+
+    private void RemoveItem(GameObject item)
+    {
+        if (item != null)
+        {
+            item.SetActive(false); // Make the item invisible and non-interactable
+        }
+    }
+
+    private void DisplayMistake(string mistake)
+    {
+        if (mistakeText != null)
+        {
+            mistakeText.text += mistake; // Append mistake
+        }
+
+     
+    }
+
+    private void DisplayMessage(string message)
+    {
+        if (messageText != null)
+        {
+            messageText.text = message;
+        }
+
     }
 }
