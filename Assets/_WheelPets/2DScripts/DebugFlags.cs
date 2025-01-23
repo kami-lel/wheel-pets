f1c45aa3cc1809fc657d8972f725d7dd422092d5
kami-lel <77594253+kami-lel@users.noreply.github.com>
2025-01-23 11:02:31 -0800

    define debug flags

---
+++ Assets/_WheelPets/2DScripts/DebugFlags.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DebugFlags", menuName = "Scriptable Objects/DebugFlags")]
+public class DebugFlags : ScriptableObject
+{
+    public bool EnableKeyboardEventSimulator = false;
+    
+}
