86328ff93c41f2a66bdf68f1133402b4a29f5950
joshwidjaja <josh@widjaja.us>
2025-01-28 22:45:06 -0800

    added additional text to format

--- Assets/_WheelPets/2DScripts/LeaderboardManager.cs
+++ Assets/_WheelPets/2DScripts/LeaderboardManager.cs
@@ -38,9 +38,9 @@ public class LeaderboardManager : MonoBehaviour
         {
             GameObject newEntry = Instantiate(leaderboardEntryPrefab, entryContainer);
             newEntry.transform.Find("LeaderboardContent").GetComponent<HorizontalLayoutGroup>().enabled = true;
-            newEntry.transform.Find("LeaderboardContent/RankText").GetComponent<TextMeshProUGUI>().text = (i + 1).ToString();
-            newEntry.transform.Find("LeaderboardContent/NameText").GetComponent<TextMeshProUGUI>().text = sortedEntries[i].playerName;
-            newEntry.transform.Find("LeaderboardContent/ScoreText").GetComponent<TextMeshProUGUI>().text = sortedEntries[i].score.ToString();
+            newEntry.transform.Find("LeaderboardContent/RankText").GetComponent<TextMeshProUGUI>().text = "#" + (i + 1).ToString();
+            newEntry.transform.Find("LeaderboardContent/NameText").GetComponent<TextMeshProUGUI>().text = sortedEntries[i].playerName + ": ";
+            newEntry.transform.Find("LeaderboardContent/ScoreText").GetComponent<TextMeshProUGUI>().text = sortedEntries[i].score.ToString() + " Points";
         }
     }
 
