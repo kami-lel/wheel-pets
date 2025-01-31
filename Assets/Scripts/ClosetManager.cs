0a78e782759b769ec6d69ac35042055f56b9c458
quinlanhoang <qbhoang@ucsc.edu>
2025-01-30 19:51:14 -0800

    UI Inventory

--- Assets/Scripts/ClosetManager.cs
+++ Assets/Scripts/ClosetManager.cs
@@ -2,8 +2,27 @@ using UnityEngine;
 
 public class ClosetManager : MonoBehaviour
 {
+   public ItemSlot[] itemSlot;
+
    public void AddItem(string itemName, int quantity, Sprite itemSprite)
    {
-    Debug.Log("itemName = " + itemName + "quantity =" + quantity + "itemSprite =" + itemSprite);
+      for (int i = 0; i < itemSlot.Length; i++)
+      {
+         if(itemSlot[i].isFull == false)
+         {
+            itemSlot[i].AddItem(itemName, quantity, itemSprite);
+            return;
+         }
+      }
+      Debug.Log("itemName = " + itemName + "quantity =" + quantity + "itemSprite =" + itemSprite);
    } 
+
+   public void DeselectAllSlots()
+   {
+      for (int i = 0; i < itemSlot.Length; i++)
+      {
+         itemSlot[i].selectedShader.SetActive(false);
+         itemSlot[i].thisItemSelected = false;
+      }
+   }
 }
