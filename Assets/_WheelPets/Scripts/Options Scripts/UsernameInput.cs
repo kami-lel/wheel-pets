05fb96cf4743bb27148d92e732a957829a7183e5
Yazmyn Sims <122718475+YazmynS@users.noreply.github.com>
2025-02-01 13:46:48 -0800

    Profiles Settings Complete.
    Holding off on Audio and Language Settings for the time being

--- Assets/_WheelPets/Scripts/Options Scripts/UsernameInput.cs
+++ Assets/_WheelPets/Scripts/Options Scripts/UsernameInput.cs
@@ -1,16 +1,52 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
 
-public class UsernameInput : MonoBehaviour
+public class UsernameInput : MonoBehaviour, IPointerClickHandler, ISelectHandler, IDeselectHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public TMP_InputField usernameInputField;
+    private string savedUsername = "";
+
     void Start()
     {
-        
+        if (usernameInputField != null)
+        {
+            usernameInputField.text = savedUsername;
+            usernameInputField.interactable = true; // Ensure it's interactable
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerClick(PointerEventData eventData)
     {
-        
+        ActivateInput();
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        ActivateInput();
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        SaveUsername();
+    }
+
+    void ActivateInput()
+    {
+        if (usernameInputField != null)
+        {
+            usernameInputField.interactable = true;
+            usernameInputField.readOnly = false; // Ensure it's not read-only
+            usernameInputField.ActivateInputField();
+        }
+    }
+
+    void SaveUsername()
+    {
+        if (usernameInputField != null)
+        {
+            savedUsername = usernameInputField.text;
+            usernameInputField.readOnly = true; // Prevent accidental editing
+        }
     }
 }
