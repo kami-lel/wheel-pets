b2f5a3e093d5cdfbbe6c9be344061e24e96607df
kami-lel <77594253+kami-lel@users.noreply.github.com>
2025-02-01 00:28:50 -0800

    add PlayerData.cs

---
+++ Assets/_WheelPets/Scripts/PlayerData.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+PlayerData playerData = new PlayerData();
+
+class PlayerData
+{
+    public static PlayerData LoadFromFile()
+    {
+        // TODO
+        return new PlayerData();
+    }
+
+    public static bool SaveToFile(PlayerData playerData)
+    {
+        // TODO
+        return false;
+    }
+}
