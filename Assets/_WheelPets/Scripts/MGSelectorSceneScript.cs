3c8c0d4bc86fa8deb4851fcda60e295e3631b3cd
kami-lel <77594253+kami-lel@users.noreply.github.com>
2025-02-02 18:28:54 -0800

    update MGSelectorSceneScript and scene button callbacks

---
+++ Assets/_WheelPets/Scripts/MGSelectorSceneScript.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MGSelectorSceneScript : MonoBehaviour
+{
+    public void BackButtonOnClick()
+    {
+        SceneManager.LoadScene("PlayScene");
+    }
+
+    public void DogWalkButtonOnClick()
+    {
+        // TODO
+    }
+
+    public void DogBathButtonOnClick()
+    {
+        // TODO
+    }
+
+    public void TugOWarButtonButtonOnClick()
+    {
+        // TODO
+    }
+
+    public void FetchButtonOnClick()
+    {
+        // TODO
+    }
+}
