74d9be8e09310adfd9fa473f20c68f895452aa34
Anthony Angeles <aneangel@ucsc.edu>
2025-02-08 05:59:29 -0800

    updated the logic to include the purchase item and subtract driving points

--- Assets/_WheelPets/Scripts/Store/BuyScreenMenu.cs
+++ Assets/_WheelPets/Scripts/Store/BuyScreenMenu.cs
@@ -8,12 +8,46 @@ public class BuyScreenMenu : MonoBehaviour
     private int costu;
     public void Enable(string name, int cost)
     {
+        itemSelected = name;
         name_text.text = "Purchase " + name + "?";
         costu = cost;
+        this.gameObject.SetActive(true);
     }
 
+    // Call this method when the cancel button is pressed
+    public void CancelPurchase()
+    {
+        Disable();
+    }
+
+    public void ConfirmPurchase()
+    {
+        // Check if the user has enough driving points
+        if (PlayerData.Data.drivingPoint >= costu)
+        {
+            // if enoguh points subtract cost
+            PlayerData.Data.drivingPoint -= costu;
+
+            // Optional confirm message
+            Debug.Log("Purchased " + itemSelected + " for " + costu + " driving point");
+
+            // Save updated player data to file
+            PlayerData.SaveToFile();
+        }
+        else
+        {
+            // Inform that user doesn't have enough points
+            Debug.Log("Not enough driving point");
+        }
+        
+        // Disable the purchase screen regardless of purchase
+        Disable();
+    }
+    
     public void Disable()
     {
         this.gameObject.SetActive(false);
     }
+    
+    
 }
