004395d7c3a2e10a51165b168120958c64faed87
kami-lel <77594253+kami-lel@users.noreply.github.com>
2025-02-09 13:48:42 -0800

    organize selector scene scripts

--- Assets/_WheelPets/Scripts/MG_SelectorScene/UnlockManager.cs
+++ Assets/_WheelPets/Scripts/MG_SelectorScene/UnlockManager.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class MGUnlockManager : MonoBehaviour
 {
-    [SerializeField] public List<MGUnlock> MGUnlocks;
-    public GameObject MGContainer; 
+    [SerializeField]
+    public List<MGUnlock> MGUnlocks;
+    public GameObject MGContainer;
     private PlayerData playerData;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerData = PlayerData.Data;
 
-        foreach(Transform MG in MGContainer.transform)
+        foreach (Transform MG in MGContainer.transform)
         {
             if (MG.gameObject.tag == "Unlockable MG")
             {
