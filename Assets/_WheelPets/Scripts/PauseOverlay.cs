a6e8b94631b73aff5e59c8c1f6fa590ddbcf8219
kami-lel <77594253+kami-lel@users.noreply.github.com>
2025-02-10 00:27:39 -0800

    add UI elements

--- Assets/_WheelPets/Scripts/PauseOverlay.cs
+++ Assets/_WheelPets/Scripts/PauseOverlay.cs
@@ -5,6 +5,12 @@ public class PauseOverlay : MonoBehaviour
     [SerializeField]
     private GameObject container;
 
+    // UI interface functions
+    public void PauseButtonOnClick()
+    {
+        // TOOD
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
