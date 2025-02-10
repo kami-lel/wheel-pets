1a9a8545d6b1a549a13527d77298cc8c1ff72835
kami-lel <77594253+kami-lel@users.noreply.github.com>
2025-02-10 15:51:20 -0800

    make gameover possible for Fetch

--- Assets/_WheelPets/Scripts/MGShared/GameOverManager.cs
+++ Assets/_WheelPets/Scripts/MGShared/GameOverManager.cs
@@ -5,13 +5,17 @@ public class GameOverManager : MonoBehaviour
 {
     public GameObject gameOverPrefab;
     private GameObject gameOverInstance;
-    public GameObject panelObject;   
+    public GameObject panelObject;
+
     void Start()
     {
         // create prefab if it doesn't exist
         if (gameOverInstance == null)
         {
-            gameOverInstance = Instantiate(gameOverPrefab, parent:panelObject.transform);
+            gameOverInstance = Instantiate(
+                gameOverPrefab,
+                parent: panelObject.transform
+            );
             gameOverInstance.SetActive(false);
         }
     }
