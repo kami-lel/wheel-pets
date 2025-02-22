017b2d1167414b226ddcec9ac81d310ad426b410
kami-lel <77594253+kami-lel@users.noreply.github.com>
2025-02-21 17:45:50 -0800

    add pet accessory enum and update player adoption comment

--- Assets/_WheelPets/Scripts/DataSystem/PlayerData.cs
+++ Assets/_WheelPets/Scripts/DataSystem/PlayerData.cs
@@ -62,7 +62,7 @@ public class PlayerData
     public MinigameStatistics statWalkScene = new();
 
     // pet's data
-    public bool hasAdoptPet = false;
+    public bool hasAdoptPet = false; // TODO make adoptionn scene working
     public PetData petData = new();
 
     // declare classes
@@ -82,6 +82,27 @@ public class PlayerData
         public int winCount = 0;
     }
 
+    /// <summary>
+    /// Data type for pet accessories.
+    /// Renderable in PetPrefab, purchasable in Store, and usable in Closet.
+    /// </summary>
+    [Serializable]
+    public enum PetAccessory
+    {
+        None, // default value
+        Atelier,
+        CloudyGlasses,
+        DeliveryCap,
+        HawaiianFlower,
+        Leaf,
+        PiratePatch,
+        PrettyBow,
+        RainbowHeadband,
+        StarCowlo,
+        UnicornHorn,
+        Xddcc,
+    }
+
     [Serializable]
     public class PetData
     {
@@ -89,5 +110,6 @@ public class PlayerData
         public int animalType = 0;
         public float dominantColorHue = 0f;
         public float secondaryColorHue = 0f;
+        public PetAccessory currentAccessory = PetAccessory.None;
     }
 }
