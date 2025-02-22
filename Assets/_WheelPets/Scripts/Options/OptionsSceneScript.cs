7998788a4e449f3ad392e5703e5cf3e6469e483b
kami-lel <77594253+kami-lel@users.noreply.github.com>
2025-02-21 20:34:43 -0800

    update comments for bug reports and new features in OptionsSceneScript.cs

--- Assets/_WheelPets/Scripts/Options/OptionsSceneScript.cs
+++ Assets/_WheelPets/Scripts/Options/OptionsSceneScript.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-// bug pet prefab is not properly rendered
+// BUG pet prefab is not properly rendered
+// BUG should disallow change pet after first adopted it
+// TODO option to re-adopt a new pet (accessory is kept though)
+// TODO reset all data option
 public class OptionsSceneScript : MonoBehaviour
 {
     // todo connect more buttons, etc.
