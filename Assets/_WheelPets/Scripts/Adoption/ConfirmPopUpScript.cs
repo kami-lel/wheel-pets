ca6c04fffa3580163de19e03ebb5c340cea04a61
kami-lel <77594253+kami-lel@users.noreply.github.com>
2025-02-21 21:01:35 -0800

    add input field for pet name and update button functionality

--- Assets/_WheelPets/Scripts/Adoption/ConfirmPopUpScript.cs
+++ Assets/_WheelPets/Scripts/Adoption/ConfirmPopUpScript.cs
@@ -1,15 +1,24 @@
+using TMPro;
 using UnityEngine;
 
 // fixme improve ui placement & design
 public class ConfirmPopUpScript : MonoBehaviour
 {
+    [SerializeField]
+    private TMP_InputField petNameField;
+
     public void ConfirmButtonOnClick()
     {
         // TODO
+        if (petNameField.text != "")
+        {
+            Data.GetPlayerData().petData.name = petNameField.text;
+        }
     }
 
     public void CancelButtonOnClick()
     {
-        // TODO
+        // Disable this game object by setting active to false
+        gameObject.SetActive(false);
     }
 }
