c297030628c973897829f33764e68105bd738ead
Mako1688 <128003121+Mako1688@users.noreply.github.com>
2025-02-22 17:12:22 -0800

    Fixing Bath Scene

--- Assets/_WheelPets/Scripts/MGBath/BathDraggables.cs
+++ Assets/_WheelPets/Scripts/MGBath/BathDraggables.cs
@@ -1,25 +1,21 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class BathDraggables
-    : MonoBehaviour,
-        IPointerDownHandler,
-        IPointerUpHandler,
-        IDragHandler
+public class BathDraggables : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
-    private RectTransform rectTransform;
-    private Canvas canvas;
     private bool isDragging = false;
+    private Vector3 originalPosition;
+    private Vector3 offset;
 
     void Start()
     {
-        canvas = GetComponentInParent<Canvas>();
-        rectTransform = GetComponent<RectTransform>();
+        originalPosition = transform.position;
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         isDragging = true;
+        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(eventData.position.x, eventData.position.y, 0));
     }
 
     public void OnPointerUp(PointerEventData eventData)
@@ -27,31 +23,44 @@ public class BathDraggables
         isDragging = false;
 
         BathGame bathGame = FindFirstObjectByType<BathGame>();
-        if (
-            bathGame != null
-            && RectTransformUtility.RectangleContainsScreenPoint(
-                bathGame.targetObject.GetComponent<RectTransform>(),
-                rectTransform.position,
-                canvas.worldCamera
-            )
-        )
+        if (bathGame != null)
         {
-            bathGame.NotifyItemDragged(gameObject.tag);
+            Collider2D targetCollider = bathGame.targetObject.GetComponent<Collider2D>();
+            Collider2D itemCollider = GetComponent<Collider2D>();
+
+            if (targetCollider != null && itemCollider != null)
+            {
+                bool isColliding = itemCollider.IsTouching(targetCollider);
+
+                Debug.Log("Collision check: " + isColliding);
+
+                if (isColliding)
+                {
+                    bathGame.NotifyItemDragged(gameObject.tag);
+                }
+                else
+                {
+                    // Return the item to its original position if not colliding
+                    transform.position = originalPosition;
+                }
+            }
+            else
+            {
+                Debug.LogError("Collider2D component not found on target object or item!");
+            }
+        }
+        else
+        {
+            Debug.LogError("BathGame script not found!");
         }
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        if (isDragging && canvas != null)
+        if (isDragging)
         {
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(
-                canvas.transform as RectTransform,
-                eventData.position,
-                canvas.worldCamera,
-                out Vector2 position
-            );
-
-            rectTransform.localPosition = position;
+            Vector3 newPosition = Camera.main.ScreenToWorldPoint(new Vector3(eventData.position.x, eventData.position.y, 0)) + offset;
+            transform.position = new Vector3(newPosition.x, newPosition.y, originalPosition.z);
         }
     }
 }
