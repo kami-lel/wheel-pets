7d028e24ed450e77c29648e0227010e07c1404fb
kami-lel <77594253+kami-lel@users.noreply.github.com>
2025-02-23 12:27:42 -0800

    update various fixme and bug comments in scripts across the project

--- Assets/_WheelPets/Scripts/MGFetch/FetchSceneScript.cs
+++ Assets/_WheelPets/Scripts/MGFetch/FetchSceneScript.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-// BUG replace static pet with PetPrebab
+// BUG marco replace static pet with PetPrebab
 // todo add more instruction for how to play the game
 // todo add high score function
 // fixme common buttons: back, pause, etc. should share an uniform design language / placement across scenes
-// FIXME use PauseOverlay to handle game start/pause/win/loss, remove GameOverPanel
+// FIXME josh use PauseOverlay to handle game start/pause/win/loss, remove GameOverPanel
 public class FetchSceneScript : MonoBehaviour
 {
     public void BackButtonOnClick()
