51d0e0d774d5c5fc4e39f7b0a5c9ee4e1b27060a
kami-lel <77594253+kami-lel@users.noreply.github.com>
2025-02-26 15:40:31 -0800

    update MinigameStatistics to track best score with currentScore parameter

--- Assets/_WheelPets/Scripts/DataSystem/StatManager.cs
+++ Assets/_WheelPets/Scripts/DataSystem/StatManager.cs
@@ -4,31 +4,50 @@ using System;
 
 public class MinigameStatistics
 {
-    public void RecordWin()
+    public bool RecordWin(float currentScore)
     {
         playCount++;
         winCount++;
+        return UpdateBestScore(currentScore);
     }
 
-    public void RecordLose()
+    public bool RecordLose(float currentScore)
     {
         playCount++;
+        return UpdateBestScore(currentScore);
     }
 
-    public int playCount = 0; { get; private set; }
-    public int winCount = 0; { get; private set; }
-    public float bestScore = 0f; { get; private set; }
+    public int playCount = 0;
+    public int winCount = 0;
+    public float bestScore = 0f;
 
-    public string bestScoreUnit { get; private set; }
-    private bool isBestScoreReversed; // determine high score direction
+    public string bestScoreUnit;
+    private readonly bool isBestScoreReversed;
 
-    public MinigameStatistics(
-        string bestScoreUnit = "Point",
-        bool isBestScoreReversed = false
-    )
+    public MinigameStatistics(string bestScoreUnit = "Point", bool isBestScoreReversed = false)
     {
         this.bestScoreUnit = bestScoreUnit;
         this.isBestScoreReversed = isBestScoreReversed;
     }
 
+    private bool UpdateBestScore(float currentScore)
+    {
+        if (isBestScoreReversed)
+        {
+            if (currentScore < bestScore)
+            {
+                bestScore = currentScore;
+                return true;
+            }
+        }
+        else
+        {
+            if (currentScore > bestScore)
+            {
+                bestScore = currentScore;
+                return true;
+            }
+        }
+        return false;
+    }
 }
