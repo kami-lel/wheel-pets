6c03549ec459cf6e1a275d871bf193ee7eda9b28
kami-lel <77594253+kami-lel@users.noreply.github.com>
2025-03-04 15:47:19 -0800

    implement auto-save

--- Assets/_WheelPets/Scripts/DataSystem/DataSystemPrefabScript.cs
+++ Assets/_WheelPets/Scripts/DataSystem/DataSystemPrefabScript.cs
@@ -2,9 +2,25 @@ using UnityEngine;
 
 public class DataSystemPrefabScript : MonoBehaviour
 {
+    [SerializeField]
+    private float autoSaveIntervalSec = 30f; // Auto-save interval in seconds
+
+    private float lastSaveTime = 0f; // Last save time tracker
+
     void Update()
     {
-        // todo timed auto-save
+        // Check if the difference between the current time and the last save time
+        // is greater than or equal to the auto-save interval
+        if (Time.time - lastSaveTime >= autoSaveIntervalSec)
+        {
+            Data.SavePlayerDataToFile(); // Perform the auto-save
+            lastSaveTime = Time.time; // Update last save time
+
+            if (Debug.isDebugBuild)
+            {
+                Debug.Log("Data\tAuto-Saved");
+            }
+        }
     }
 
     void OnApplicationQuit()
