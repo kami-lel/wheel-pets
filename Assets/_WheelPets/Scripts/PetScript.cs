573f6190ec0fe7418ea1569ba9440af1afaf4bbf
kami-lel <77594253+kami-lel@users.noreply.github.com>
2025-03-13 14:43:23 -0700

    refactor PetScript for cleaner code and improved readability
    PetScript.cs: condense multi-line statements into single-line expressions for clarity

--- Assets/_WheelPets/Scripts/PetScript.cs
+++ Assets/_WheelPets/Scripts/PetScript.cs
@@ -107,60 +107,18 @@ public class PetScript : MonoBehaviour
         {
             case PlayerData.AnimalType.Dog:
             default:
-                dominantColor = CreateColorDom(
-                    -0.06f,
-                    0.15f,
-                    0.8f,
-                    0.65f,
-                    0.7f,
-                    0.9f
-                );
-                secondaryColor = CreateColorSec(
-                    0f,
-                    1f,
-                    0.1f,
-                    0.2f,
-                    1.0f,
-                    1.0f
-                );
+                dominantColor = CreateColorDom(-0.06f, 0.15f, 0.8f, 0.65f, 0.7f, 0.9f);
+                secondaryColor = CreateColorSec(0f, 1f, 0.1f, 0.2f, 1.0f, 1.0f);
                 break;
 
             case PlayerData.AnimalType.Cat:
-                dominantColor = CreateColorSec(
-                    -0.06f,
-                    0.15f,
-                    0.8f,
-                    0.65f,
-                    0.7f,
-                    0.9f
-                );
-                secondaryColor = CreateColorDom(
-                    0f,
-                    1f,
-                    0.1f,
-                    0.2f,
-                    1.0f,
-                    1.0f
-                );
+                dominantColor = CreateColorSec(-0.06f, 0.15f, 0.8f, 0.65f, 0.7f, 0.9f);
+                secondaryColor = CreateColorDom(0f, 1f, 0.1f, 0.2f, 1.0f, 1.0f);
                 break;
 
             case PlayerData.AnimalType.Rabbit:
-                dominantColor = CreateColorDom(
-                    -0.06f,
-                    0.15f,
-                    0.8f,
-                    0.65f,
-                    0.7f,
-                    0.9f
-                );
-                secondaryColor = CreateColorSec(
-                    0f,
-                    1f,
-                    0.1f,
-                    0.2f,
-                    1.0f,
-                    1.0f
-                );
+                dominantColor = CreateColorDom(-0.06f, 0.15f, 0.8f, 0.65f, 0.7f, 0.9f);
+                secondaryColor = CreateColorSec(0f, 1f, 0.1f, 0.2f, 1.0f, 1.0f);
                 break;
         }
 
@@ -188,16 +146,7 @@ public class PetScript : MonoBehaviour
     )
     {
         float value = data.petData.dominantColorHue;
-        return CreateColorFromRange(
-            value,
-            hBegin,
-            hEnd,
-            sBegin,
-            sEnd,
-            vBegin,
-            vEnd,
-            true
-        );
+        return CreateColorFromRange(value, hBegin, hEnd, sBegin, sEnd, vBegin, vEnd, true);
     }
 
     private Color CreateColorSec(
@@ -210,16 +159,7 @@ public class PetScript : MonoBehaviour
     )
     {
         float value = data.petData.secondaryColorHue;
-        return CreateColorFromRange(
-            value,
-            hBegin,
-            hEnd,
-            sBegin,
-            sEnd,
-            vBegin,
-            vEnd,
-            false
-        );
+        return CreateColorFromRange(value, hBegin, hEnd, sBegin, sEnd, vBegin, vEnd, false);
     }
 
     private Color CreateColorFromRange(
@@ -239,11 +179,7 @@ public class PetScript : MonoBehaviour
 
         if (Debug.isDebugBuild)
         {
-            Debug.Log(
-                "Pet\t"
-                    + (isDom ? "Dominant" : "Secondary")
-                    + $"Color\th:{h} s:{s} v:{v}"
-            );
+            Debug.Log("Pet\t" + (isDom ? "Dominant" : "Secondary") + $"Color\th:{h} s:{s} v:{v}");
         }
 
         return Color.HSVToRGB(h, s, v);
@@ -286,28 +222,20 @@ public class PetScript : MonoBehaviour
         };
 
         // loop via all possible accessories in enum AccessoryType
-        foreach (
-            AccessoryType accessory in Enum.GetValues(typeof(AccessoryType))
-        )
+        foreach (AccessoryType accessory in Enum.GetValues(typeof(AccessoryType)))
         {
             // find accessory as game object
             string accessoryName = accessory.ToString();
-            Transform accessoryTransform =
-                activePetAccessoryGroup.transform.Find(accessoryName);
+            Transform accessoryTransform = activePetAccessoryGroup.transform.Find(accessoryName);
             if (accessoryTransform != null)
             {
                 // set active condition
-                bool isWearing = data.petData.currentAccessories.Contains(
-                    accessory
-                );
+                bool isWearing = data.petData.currentAccessories.Contains(accessory);
                 accessoryTransform.gameObject.SetActive(isWearing);
             }
             else if (Debug.isDebugBuild)
             {
-                Debug.LogWarning(
-                    "PetPrefab\tcan't find accessory as game object:"
-                        + accessoryName
-                );
+                Debug.LogWarning("PetPrefab\tcan't find accessory as game object:" + accessoryName);
             }
         }
     }
